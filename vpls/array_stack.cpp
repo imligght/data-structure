6826ffb805a6165a41d780eae5053caeb0c0d1a5
light <caiocr610@gmail.com>
2024-03-30 20:41:33 -0300

    adicionando os exercícios de stack

---
+++ vpls/array_stack.cpp
@@ -0,0 +1,129 @@
+// Copyright [2019] <COLOQUE SEU NOME AQUI...>
+#ifndef STRUCTURES_ARRAY_STACK_H
+#define STRUCTURES_ARRAY_STACK_H
+
+#include <cstdint>  // std::size_t
+#include <stdexcept>  // C++ exceptions
+
+namespace structures {
+
+template<typename T>
+//! CLASSE PILHA
+class ArrayStack {
+ public:
+    //! construtor simples
+    ArrayStack();
+    //! construtor com parametro tamanho
+    explicit ArrayStack(std::size_t max);
+    //! destrutor
+    ~ArrayStack();
+    //! metodo empilha
+    void push(const T& data);
+    //! metodo desempilha
+    T pop();
+    //! metodo retorna o topo
+    T& top();
+    //! metodo limpa pilha
+    void clear();
+    //! metodo retorna tamanho
+    std::size_t size();
+    //! metodo retorna capacidade maxima
+    std::size_t max_size();
+    //! verifica se esta vazia
+    bool empty();
+    //! verifica se esta cheia
+    bool full();
+
+ private:
+    T* contents;
+    int top_;
+    std::size_t max_size_;
+
+    static const auto DEFAULT_SIZE = 10u;
+};
+
+}  // namespace structures
+
+#endif
+
+
+template<typename T>
+structures::ArrayStack<T>::ArrayStack() {
+    max_size_ = DEFAULT_SIZE;
+    contents = new T[max_size_];
+    top_ = -1;
+}
+
+template<typename T>
+structures::ArrayStack<T>::ArrayStack(std::size_t max) {
+    // COLOQUE SEU CODIGO AQUI...
+    max_size_ = max;
+    contents = new T[max];
+    top_ = -1;
+}
+
+template<typename T>
+structures::ArrayStack<T>::~ArrayStack() {
+    delete [] contents;
+}
+
+template<typename T>
+void structures::ArrayStack<T>::push(const T& data) {
+    if (full()) {
+        throw std::out_of_range("pilha cheia");
+    } else {
+        // COLOQUE SEU CODIGO AQUI...
+        top_++;
+        contents[top_] = data;
+    }
+}
+
+template<typename T>
+T structures::ArrayStack<T>::pop() {
+    // COLOQUE SEU CODIGO AQUI...
+    if (top_ == -1) {
+        throw std::out_of_range("pilha vazia");
+    }
+    top_--;
+    return contents[top_ + 1];
+}
+
+template<typename T>
+T& structures::ArrayStack<T>::top() {
+    // COLOQUE SEU CODIGO AQUI...
+    return contents[top_];
+}
+
+template<typename T>
+void structures::ArrayStack<T>::clear() {
+    // COLOQUE SEU CODIGO AQUI...
+    top_ = -1;
+}
+
+template<typename T>
+std::size_t structures::ArrayStack<T>::size() {
+    // COLOQUE SEU CODIGO AQUI...
+    return top_ + 1;
+}
+
+template<typename T>
+std::size_t structures::ArrayStack<T>::max_size() {
+    // COLOQUE SEU CODIGO AQUI...
+    return max_size_;
+}
+
+template<typename T>
+bool structures::ArrayStack<T>::empty() {
+    // COLOQUE SEU CODIGO AQUI...
+    return top_ == -1;
+}
+
+template<typename T>
+bool structures::ArrayStack<T>::full() {
+    // COLOQUE SEU CODIGO AQUI...
+    return top_ + 1 == static_cast<int>(max_size_);
+}
+
+int main() {
+    ;
+}
